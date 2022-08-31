0b489f23c969f8f301a42d1fc10f2bd42cd2f719
Wesley <89660042+WesTheHuman2000@users.noreply.github.com>
2022-08-31 19:32:53 -0400

    exercise 8 fix

--- exercise_08.py
+++ exercise_08.py
@@ -1,13 +1,6 @@
 number_list = []
 unique_nums = []
-for i in range(10):
-    user_input = int(input(f'Enter number {i+1}: '))
-    number_list.append(user_input)
 
-#for x in range(number_list):
-#    if user_input[x] %2 == 0:
-
-print(set(number_list))
 
 for elem in number_list:
     if number_list.count(elem)==1:
