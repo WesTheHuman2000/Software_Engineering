b63f4d4cea3ae038182d5935ba1e4672e1a42d2b
Wesley <89660042+WesTheHuman2000@users.noreply.github.com>
2022-08-31 19:31:28 -0400

    fixing activity 8

--- exercise_08.py
+++ exercise_08.py
@@ -1,5 +1,5 @@
 number_list = []
-
+unique_nums = []
 for i in range(10):
     user_input = int(input(f'Enter number {i+1}: '))
     number_list.append(user_input)
@@ -8,3 +8,9 @@ for i in range(10):
 #    if user_input[x] %2 == 0:
 
 print(set(number_list))
+
+for elem in number_list:
+    if number_list.count(elem)==1:
+        unique_nums.append(elem)
+
+print(unique_nums)
